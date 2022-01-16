8996b7655ada90355f5d95ed9809a28d0b2375db
said017 <saidam017@gmail.com>
2022-01-16 19:11:33 +0700

    bismillah 10 m

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -9,12 +9,12 @@ const data = {
     },
     {
       title: 'How Much is Metaverse Workers NFT?',
-      content: `For the Whitelist Private Sale the price will be 0,1 ETH + gas price and you will be able to mint a max of 3 Workers. For the Public Sale the price will be 0,15 ETH + gas price and you will be able to mint a max of 5 Workers.`,
+      content: `For the Whitelist Private Sale the price will be 40 MATIC + gas price and you will be able to mint a max of 3 Workers. For the Public Sale the price will be 40 MATIC + gas price and you will be able to mint a max of 5 Workers per transaction.`,
     },
     {
       title: 'How do I purchase an NFT?',
       content:
-        'First, you will need to purchase Ethereum (ETH) from a cryptocurrency exchange such as Coinbase, Binance, or Gemini. Then, you will need to transfer your ETH into a digital wallet, the most popular being Metamask. You will be able to connect this wallet to the Metaverse Workers website to purchase your NFT.',
+        'First, you will need to purchase MATIC from a cryptocurrency exchange such as Coinbase, Binance, or Gemini. Then, you will need to transfer your MATIC into a digital wallet, the most popular being Metamask. You will be able to connect this wallet to the Metaverse Workers website to purchase your NFT.',
     },
     {
       title: 'What is the benefit for the owner of Metaverse Workers NFT?',
@@ -23,7 +23,7 @@ const data = {
     {
       title: 'Do I get full rights on my NFTs?',
       content:
-        'Yes, In the sum of eth price not including transaction fees, The Metaverse Workers hereby transfers to purchasers full title and all rights and interests with 2.5% secondary royalty, You will be the owner of the nft and it is absolutely 1/1 & Secured on ERC-72 token.',
+        'Yes, In the sum of eth price not including transaction fees, The Metaverse Workers hereby transfers to purchasers full title and all rights and interests with 2.5% secondary royalty, You will be the owner of the nft and it is absolutely 1/1 & Secured on ERC-721 token.',
     },
   ],
 };
