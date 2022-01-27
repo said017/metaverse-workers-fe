86b255f213de4bbf0f017253cb2156d4a652d33b
said017 <saidam017@gmail.com>
2022-01-28 05:29:32 +0700

    Update MainHero.tsx

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -192,16 +192,14 @@ const MainHero = () => {
                   <s.SpacerMedium /> */}
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"></div>
                 <s.Container fd={'column'}>
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      dispatch(connect());
-                      getData();
-                    }}
-                    className={`w-180 flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
-                  >
-                    Connect
-                  </button>
+                        <a
+                          href="https://mint.metaverseworkersclub.com/"
+                          target="_blank"
+                          rel="noreferrer"
+                          className={`w-180 items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
+                        >
+                          Mint
+                        </a>
 
                   {blockchain.errorMsg !== '' ? (
                     <>
